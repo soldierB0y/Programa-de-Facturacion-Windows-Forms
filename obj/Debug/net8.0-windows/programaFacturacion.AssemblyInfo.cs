4dc18337a33ba97be9292b94b72025b6971ce417
SoldierB0y <jebatistav14@gmail.com>
2025-04-01 22:56:54 -0300

    a

--- obj/Debug/net8.0-windows/programaFacturacion.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/programaFacturacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("programaFacturacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0bf5960c78eadc3ec5c88e5ecc4b2ee05dc2de6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc9e959f3bae9b7e6ae5d82fbcca8dd1949b933")]
 [assembly: System.Reflection.AssemblyProductAttribute("programaFacturacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("programaFacturacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
